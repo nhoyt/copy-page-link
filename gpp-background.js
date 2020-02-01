ddd174d368f6ede83d2e15b5d46e7847ccc1a50d
Nicholas Hoyt <nick@webui.com>
2020-02-01 10:37:35 -0600

    Initial commit

---
+++ gpp-background.js
@@ -0,0 +1,136 @@
+/*
+*   background.js
+*/
+const defaultFormat = 'markdown';
+var options; // Initialized by startProcessing in popup.js
+
+// Generic error handler
+function onError (error) {
+  console.log(`Error: ${error}`);
+}
+
+/* ---------------------------------------------------------------- */
+
+// Called from options.js for customizing 'options saved' message
+
+#ifdef FIREFOX
+function getPlatform () {
+  function gotPlatformInfo (info) {
+    browser.runtime.sendMessage(info.os);
+  }
+  browser.runtime.getPlatformInfo().then(gotPlatformInfo, onError);
+}
+#endif
+#ifdef CHROME
+function getPlatform () {
+  chrome.runtime.getPlatformInfo(function (info) {
+    chrome.runtime.sendMessage(info.os);
+  }, onError);
+}
+#endif
+
+/* -------------------------------------------------------- */
+/*   Functions for extracting and processing link info      */
+/* -------------------------------------------------------- */
+
+function copyToClipboard (str) {
+  let listener = function (event) {
+    event.clipboardData.setData('text/plain', str);
+    event.preventDefault();
+  };
+  document.addEventListener('copy', listener);
+  document.execCommand('copy', false, null);
+  document.removeEventListener('copy', listener);
+}
+
+function getFormattedLink (data) {
+  let name = data.selection ? data.selection : data.title;
+  let format = options.format || defaultFormat;
+
+  switch (format) {
+    case 'markdown':
+      return `[${name}](${data.href})`;
+
+    case 'html':
+      return `<a href="${data.href}">${name}</a>`;
+
+    case 'latex':
+      return `\\href{${data.href}}{${name}}`;
+
+    case 'xml':
+      return `      <${options.link} ${options.href}="${data.href}">\n` +
+             `        <${options.name}>${name}</${options.name}>\n` +
+             `      </${options.link}>\n`;
+
+    default:
+      return 'Error: Unknown format option';
+  }
+}
+
+function processLinkData (data) {
+  // Truncate selection string if length exceeds maximum
+  const maxLength = 120;
+  if (data.selection && data.selection.length > maxLength) {
+    data.selection = data.selection.substring(0, maxLength) + '…';
+  }
+  copyToClipboard(getFormattedLink(data));
+}
+
+/* ---------------------------------------------------------------- */
+
+// Because we've declared a popup for the extension, we need an entry point
+// function we can call from the popup script that replicates what the
+// browserAction.onClicked event handler would have done.
+
+#ifdef FIREFOX
+function processActiveTab () {
+  function onGotActiveTab (tabs) {
+    for (let tab of tabs) {
+      // Security policy only allows us to inject the content script that
+      // accesses title and selection for pages loaded with http or https.
+      if (tab.url.indexOf('http:') === 0 || tab.url.indexOf('https:') === 0) {
+        browser.tabs.executeScript(null, { file: 'content.js' });
+      }
+      else {
+        processLinkData({ href: tab.url, title: '', selection: '' });
+      }
+    }
+  }
+
+  let querying = browser.tabs.query({currentWindow: true, active: true});
+  querying.then(onGotActiveTab, onError);
+}
+#endif
+#ifdef CHROME
+function processActiveTab () {
+  chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+    let tab = tabs[0];
+
+    if (tab.url.indexOf('http:') === 0 || tab.url.indexOf('https:') === 0) {
+      chrome.tabs.executeScript(null, { file: 'content.js' });
+    }
+    else {
+      processLinkData({ href: tab.url, title: '', selection: '' });
+    }
+  });
+}
+#endif
+
+/* ---------------------------------------------------------------- */
+
+// Listen for messages from the content script
+
+#ifdef FIREFOX
+browser.runtime.onMessage.addListener(
+  function (request, sender) {
+    processLinkData(request);
+  }
+);
+#endif
+#ifdef CHROME
+chrome.runtime.onMessage.addListener(
+  function (request, sender) {
+    processLinkData(request);
+  }
+);
+#endif
---
+++ gpp-options.js
@@ -0,0 +1,130 @@
+/*
+*   options.js
+*/
+const defaultFormat = 'markdown';
+const defaultTimeout = '3000';
+let message;
+
+// Generic error handler
+function onError (error) {
+  console.log(`Error: ${error}`);
+}
+
+// Request platform info from background script via extension messaging
+
+function onGotBackgroundPage (page) {
+  page.getPlatform();
+}
+
+#ifdef FIREFOX
+browser.runtime.getBackgroundPage().then(onGotBackgroundPage, onError);
+#endif
+
+#ifdef CHROME
+chrome.runtime.getBackgroundPage(onGotBackgroundPage, onError);
+#endif
+
+/*
+*   Set the message text to be displayed when options are saved. This function
+*   is called when the platform message is received from the background script.
+*/
+function setMessage (platform) {
+  switch (platform) {
+    case 'mac':
+      message = 'Preferences saved!';
+      break;
+    default:
+      message = 'Options saved!';
+      break;
+  }
+}
+
+/*
+*   Add event listener for background script message
+*/
+browser.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    setMessage(request);
+  }
+);
+
+/* -------------------------------------------------------- */
+/*   Functions for saving and restoring user options        */
+/* -------------------------------------------------------- */
+
+/*
+*   Save user options in browser.storage and display message
+*/
+function saveOptions(e) {
+  e.preventDefault();
+
+  // For use during development
+  if (false) {
+    let clearing = browser.storage.sync.clear();
+    return;
+  }
+
+  let formats = document.getElementById('formats');
+  let inputs = formats.getElementsByTagName('input');
+  let selectedFormat = null;
+
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].checked) {
+      selectedFormat = inputs[i].value;
+      break;
+    }
+  }
+
+  function notifyUser () {
+    let status = document.getElementById('status');
+    status.textContent = message;
+
+    setTimeout(function () {
+      status.textContent = '';
+    }, 750);
+    console.log(message);
+  }
+
+  if (selectedFormat) {
+    let setting = browser.storage.sync.set({
+      format: selectedFormat,
+      auto: document.getElementById('auto').checked,
+      msec: document.getElementById('msec').value,
+
+      link: document.getElementById('link').value,
+      href: document.getElementById('href').value,
+      name: document.getElementById('name').value
+    });
+    setting.then(notifyUser, onError);
+  }
+}
+
+/*
+*   Restore HTML form values based on user options saved in browser.storage
+*/
+function restoreOptions() {
+
+  function setPreferences (options) {
+    document.getElementById(options.format || defaultFormat).checked = true;
+
+    document.getElementById('auto').checked =
+      (typeof options.auto === 'undefined') ? true : options.auto;
+
+    document.getElementById('msec').value = options.msec || defaultTimeout;
+
+    document.getElementById('link').value = options.link || 'link';
+    document.getElementById('href').value = options.href || 'href';
+    document.getElementById('name').value = options.name || 'name';
+
+    console.log(options);
+  }
+
+  let getting = browser.storage.sync.get();
+  getting.then(setPreferences, onError);
+}
+
+/*
+*   Add event listeners for saving and restoring options
+*/
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('form').addEventListener('submit', saveOptions);
